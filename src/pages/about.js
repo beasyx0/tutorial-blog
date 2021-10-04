5f459bcd089e04dcc51499563f677c435a1a2de1
B <b_easyx@protonmail.com>
2021-10-04 12:24:01 -0400

    Finished Gatsby tutorial part 3

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,16 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
+
 
 const AboutPage = () => {
   return (
     <Layout pageTitle="About Me">
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+      <StaticImage
+        alt="Gradient background image"
+        src="../images/gradient.jpg"
+      />
     </Layout>
   )
 }
